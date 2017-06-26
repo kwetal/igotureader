4be7d016571daa046f532aa749c7e3cdf03973df
Arjen Bax <arjen.bax@duo.nl>
2017-06-26 15:00:57 +0200

    add javadoc

--- src/main/java/igotu/core/IgotuCommands.java
+++ src/main/java/igotu/core/IgotuCommands.java
@@ -11,10 +11,22 @@ public interface IgotuCommands {
     ModelCommandResponse ModelCommand();
     InternalMemoryRead2Response InternalMemoryRead2(int size);
     ReadCommandResponse ReadCommand(int size, int position);
-    UnknownWriteCommand1Response UnknownWriteCommand1(int mode); // 00 for purge, 01? for write
+
+    /**
+     *
+     * @param mode 00 for purge, 01? for write
+     * @return
+     */
+    UnknownWriteCommand1Response UnknownWriteCommand1(int mode);
     WriteCommandResponse WriteCommand(int mode, int size, int position, byte[] data);
     UnknownWriteCommand2Response UnknownWriteCommand2(int size);
-    UnknownPurgeCommand1Response UnknownPurgeCommand1(int mode); // first time called 1E, second time called 1F
+
+    /**
+     *
+     * @param mode first time called 1E, second time called 1F
+     * @return
+     */
+    UnknownPurgeCommand1Response UnknownPurgeCommand1(int mode);
     UnknownPurgeCommand2Response UnknownPurgeCommand2();
     BC4OffModeCommandResponse BC4OffModeCommand(int xx, int hh, int mm, int ll);
 }
